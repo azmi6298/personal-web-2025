7e2d2c261154100adc969fbf2f29442c99ff4af0
Muhammad Azmi <azmi.filkom@gmail.com>
2024-11-18 21:50:09 +0700

    add eslint into project

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+];
