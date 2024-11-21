e95416946851c0cf44e9eddbdd6fb005e2b81f4e
Muhammad Azmi <azmi.filkom@gmail.com>
2024-11-21 20:59:58 +0700

    migrate tailwind config from previous project

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ["prettier-plugin-astro"],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: true,
   overrides: [
     {
-      files: '*.astro',
+      files: "*.astro",
       options: {
-        parser: 'astro',
+        parser: "astro",
       },
     },
   ],
