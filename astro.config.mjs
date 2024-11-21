db288d9ef99fb9a2f22db1ebe37a33b5501a28c5
Muhammad Azmi <azmi.filkom@gmail.com>
2024-11-21 21:21:51 +0700

    implement hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    icon(),
   ],
 });
