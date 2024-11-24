02235c08104b3cf1cf2102317f752ec4ca3df885
Muhammad Azmi <azmi.filkom@gmail.com>
2024-11-24 15:19:45 +0700

    implement splide as slider component

---
+++ src/libs/splide.js
@@ -0,0 +1,21 @@
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+document.addEventListener('DOMContentLoaded', function () {
+	var splide = new Splide('.splide', {
+		type: 'loop',
+		perPage: 4,
+		arrows: false,
+		pagination: false,
+		breakpoints: {
+			1024: {
+				perPage: 4,
+			},
+			640: {
+				perPage: 3,
+			},
+		},
+	});
+	splide.mount({ AutoScroll });
+});
