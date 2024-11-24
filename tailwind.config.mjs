10a46c9d8106dc789afca23d2e9817000ab9f16f
Muhammad Azmi <azmi.filkom@gmail.com>
2024-11-24 21:22:34 +0700

    install jetbrains font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,19 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#e2e8f0",
-        "primary-inverted": "#181418",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#e2e8f0',
+				'primary-inverted': '#181418',
+				brand: '#6d28d9',
+			},
+			fontFamily: {
+				sans: ['JetBrains Mono Variable', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
 };
